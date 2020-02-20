01cb82135e1a2aa9dd0760510c6a7d4af9629113
ihgumilar <igum002@aucklanduni.ac.nz>
2020-02-20 20:55:34 +1300

    Falling into pieces

---
+++ Assets/Scripts/RandomPosition.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public Vector3[] positions;
+    public float range= 3;
+    bool pressedSpace = false;
+
+    //void Start()
+    //{
+    //    range= 3; //maxRange = 10;
+    //}
+
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    //if (other.gameObject.tag == "Player")
+    //    // PUT 
+
+    //}
+    
+    private void Update()
+    {
+        if ( Input.GetKeyDown( KeyCode.Space) && gameObject.tag == "Player" && !pressedSpace)
+        {
+            print("in fal");
+            float randomNumber_x = Random.Range( -range, range);
+            float randomNumber_y = Random.Range(4, range* 5);
+            float randomNumber_z = Random.Range(-range, range);
+
+            transform.position = new Vector3(randomNumber_x, randomNumber_y, randomNumber_z);
+            gameObject.AddComponent<BoxCollider>();
+            Rigidbody fall = GetComponent<Rigidbody>();
+            fall = gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+            fall.useGravity = true;
+            pressedSpace = true;
+        }
+    }
+}
+
+// To do
+// 1. Counting children (done)
+// 2. Tag for each child (done)
+// 3. Add rigibody for each tagged child (done)
+// 4. Try with the real design
+// 4.1. Replace the start function
+// 4.2. Assign the rigidbody
+// 4.3. Create a tag
+// 5. Add colision
+// 6. Put it back to original position
+// 7. If the distance is less, then attach it
+
+
+
+//{
+//    // Start is called before the first frame update
+//    //public Vector3[] positions;
+//    public Vector3 minRange;
+//    public Vector3 maxRange;
+
+//    void Start()
+//    {
+//        float randomNumber_x = Random.Range(minRange.x, maxRange.x);
+//        float randomNumber_y = Random.Range(minRange.y, maxRange.y);
+//        float randomNumber_z = Random.Range(minRange.z, maxRange.z);
+
+//        transform.position = new Vector3(randomNumber_x, randomNumber_y, randomNumber_z);
+//    }
+
+//    private void OnCollisionEnter(Collision other)
+//    {
+//        //if (other.gameObject.tag == "Player")
+//        //{
+
+//        }
+//    }
+
+//}
+
---
+++ Assets/Scripts/cubes.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cubes : MonoBehaviour
+{
+    //public GameObject lvl1pyramid;
+    //public GameObject newparentgameobject;
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   int numberOfChildre = lvl1pyramid.transform.childCount;
+     //   GameObject[] pyramidpieces = lvl1pyramid.gameObject.transform.all;
+        //for (int fchild = 0; fchild < pyramidpieces.Length; fchild++)
+      //      for (int fchild = 0; fchild <numberOfChildre-1; fchild++)
+       //     {
+        //    lvl1pyramid.transform.GetChild(fchild).SetParent(transform);
+        //    lvl1pyramid.transform.GetChild(fchild).gameObject.AddComponent<RandomPosition>();
+         //   pyramidpieces[fchild].AddComponent<RandomPosition>();
+         //   pyramidpieces[fchild].transform.SetParent(transform);
+         
+       // }
+
+
+        //Transform[] children;
+
+        //children = lvl1pyramid. GetComponentsInChildren< Transform>();
+        ////Debug.Log(children.Length);
+
+
+        //for (int nchild = 0; nchild < children.Length; nchild++)
+        //{
+        //    children[nchild].SetParent(transform);
+        //    children[nchild].gameObject.AddComponent<RandomPosition>();
+        //    children[nchild].gameObject.tag = "Player";
+        //    //Debug.Log("Success");
+        //}
+
+        foreach (Transform child in this.transform)
+        {
+            //child.SetParent(transform);
+            child.gameObject.AddComponent<RandomPosition>();
+            child.gameObject.tag = "Player";
+        }
+
+
+    }
+
+    //public void Update()
+    //{
+    //    Transform[] children;
+    
+    //    children = gameObject.GetComponentsInChildren<Transform>();
+    //    //Debug.Log(children.Length);
+    //    for (int nchild = 0; nchild < children.Length; nchild++)
+    //    {
+    //        children[nchild].gameObject.tag = "Player";
+    //        //Debug.Log("Success");
+    //    }
+    //}
+
+
+
+
+////ORIGINAL SCRIPT
+//public class cubes : MonoBehaviour
+//{
+//    int size = 10;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        for (int x = 0; x < size; x++)
+//            for (int y = 0; y < size; y++)
+//                for (int z = 0; z < size; z++)
+//                {
+//                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//                    go.AddComponent<RandomPosition>();
+//                    //RandomPosition rp= go.GetComponent< Ra>
+//                    go.transform.position = new Vector3(x - size / 2, y + .5f, z - size / 2);
+//                    go.transform.localScale = new Vector3(.9f, .9f, .9f);
+//                    go.transform.SetParent(transform);
+
+//                }
+//    }
+//    private void Update()
+//    {
+//        Transform[] children;
+//        children = gameObject.GetComponentsInChildren<Transform>();
+//        //Debug.Log(children.Length);
+//        for (int nchild = 0; nchild < children.Length; nchild++)
+//        {
+//            children[nchild].gameObject.tag = "Player";
+//            //Debug.Log("Success");
+//        }
+//    }
+
+
+}
